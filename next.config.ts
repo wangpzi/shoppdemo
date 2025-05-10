be521c1b78a2dc79e7a1c36b2cdb50a3b5e5bd4f
王胖子 <wpang520@163.com>
2025-05-10 18:26:06 +0800

    Cloudflare 配置修改

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  // 如果需要支持 Cloudflare Workers 环境
+  experimental: {
+    webVitalsAttribution: ['CLS', 'LCP']
+  }
 };
 
 export default nextConfig;
